8791a7139556533a29e83c57f47769233be20f24
wk821917 <38805478+wk821917@users.noreply.github.com>
2018-07-27 15:27:25 +0800

    Create predict_lstm.py

---
+++ predict_lstm.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+from six.moves import cPickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+from keras import backend as K
+from keras.models import Model, model_from_json
+from keras.layers import Input, Dense, Flatten
+
+data = pd.read_csv('wktest-master/predict_data.csv')
+f = open('training-o3hdtoFiR/model/lstm.json', 'r')
+json_string = f.read()
+f.close()
+
+model = model_from_json(json_string)
+model.load_weights('training-o3hdtoFiR/weight/weight.hdf5')
+
+print(json_string)
