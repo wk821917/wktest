aa12cef8ad909159a85923f3cec75752ce6555ad
wk821917 <38805478+wk821917@users.noreply.github.com>
2018-07-27 17:15:10 +0800

    Update predict_lstm.py

--- predict_lstm.py
+++ predict_lstm.py
@@ -13,11 +13,11 @@ from keras.models import Model, model_from_json
 from keras.layers import Input, Dense, Flatten
 
 data = pd.read_csv('wktest-master/predict_data.csv')
-f = open('lstm.json', 'r')
+f = open('wktest-master/lstm.json', 'r')
 json_string = f.read()
 f.close()
 
 model = model_from_json(json_string)
-model.load_weights('weight.hdf5')
+model.load_weights('wktest-master/weight.hdf5')
 
 print(json_string)
