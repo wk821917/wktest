7909df16e40dda13f0b19758c06691d8f6b96ddc
wk821917 <38805478+wk821917@users.noreply.github.com>
2018-07-30 13:08:47 +0800

    Update predict_lstm.py

--- predict_lstm.py
+++ predict_lstm.py
@@ -12,6 +12,14 @@ from keras import backend as K
 from keras.models import Model, model_from_json
 from keras.layers import Input, Dense, Flatten
 
+result_file = 'result.csv'
+if environ.get('RESULT_DIR') is not None:
+    output_result_folder = os.path.join(os.environ["RESULT_DIR"], "predict_result")
+    output_result_path = os.path.join(output_result_folder, result_file)
+else:
+    output_result_folder = "predict_result"
+    output_result_path = os.path.join("predict_result", model_filename)
+    
 data = pd.read_csv('wktest-master/predict_data.csv')
 f = open('wktest-master/lstm.json', 'r')
 json_string = f.read()
@@ -32,3 +40,7 @@ x.append(data)
 result = model.predict(np.array(x))
 print(result)
 print(result.shape)
+
+result_lst = result[0]
+result_df = DataFrame({'predict':result_lst})
+result_df.to_csv(output_result_path)
