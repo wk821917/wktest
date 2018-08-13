30b7385cf8503e78d8aef1876047b9def40fdbd6
wk821917 <38805478+wk821917@users.noreply.github.com>
2018-08-13 16:16:33 +0800

    Update lstm_pred_pro.py

--- lstm_pred_pro.py
+++ lstm_pred_pro.py
@@ -45,7 +45,7 @@ os.makedirs(output_json_folder, exist_ok=True)
 '''
 get data
 '''
-req = urllib.Request('http://openapi.ecois.info/v2/poi/device/data?sn=18031400075227&nodes=1,2,3&params=82,182,185&begin=20180615&end=20180801',headers=headers)
+req = urllib.request.Request('http://openapi.ecois.info/v2/poi/device/data?sn=18031400075227&nodes=1,2,3&params=82,182,185&begin=20180615&end=20180801',headers=headers)
 res_data = urllib.urlopen(req)
 res = res_data.read()
 st = json.loads(res)
