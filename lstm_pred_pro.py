c488d8d5af93ad0026c22d5cc9f58e1ef3667939
wk821917 <38805478+wk821917@users.noreply.github.com>
2018-08-21 09:53:33 +0800

    Update lstm_pred_pro.py

--- lstm_pred_pro.py
+++ lstm_pred_pro.py
@@ -62,14 +62,16 @@ for i in range(len(data1.index)):
 print(key_num)
 data_60 = data1.iloc[key_num:key_num+1000,:]
 for i in range(data_60.shape[0]):
-    if len(data_60.iloc[i,0]['1'])==2:
+    if (len(data_60.iloc[i,0]['1'])==2) and (len(data_60.iloc[i,0])==2):
         xtilt_list.append(float(data_60.iloc[i,0]['1']['182']))
         ytilt_list.append(float(data_60.iloc[i,0]['1']['185']))
-print(len(xtilt_list),len(ytilt_list))
-for i in range(data_60.shape[0]):
-    if len(data_60.iloc[i,0])==2:
         water_percent2.append(float(data_60.iloc[i,0]['2']['82']))
         water_percent3.append(float(data_60.iloc[i,0]['3']['82']))
+print(len(xtilt_list),len(ytilt_list))
+#for i in range(data_60.shape[0]):
+#    if len(data_60.iloc[i,0])==2:
+#        water_percent2.append(float(data_60.iloc[i,0]['2']['82']))
+#        water_percent3.append(float(data_60.iloc[i,0]['3']['82']))
 print(len(water_percent2),len(water_percent3))
 data = pd.DataFrame({'xtilt':xtilt_list,'ytilt':ytilt_list,'water2':water_percent2,'water3':water_percent3})
 #data.iloc[10:,:].to_csv(os.path.join(output_result_folder,'input_data.csv'))
